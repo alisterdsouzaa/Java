1f307ce0f9c117a1bebe082999822155686bcaac
Alister <alisterdsouzaa@outlook.com>
2022-11-26 19:25:07 +0530

    Added Pattern Programs

---
+++ Pattern/Pattern16.java
@@ -0,0 +1,49 @@
+/////////////////////////////////////////////////////////////////////////
+// Triangle Pattern 16 : Accept num of row and column and display pattern
+/////////////////////////////////////////////////////////////////////////
+// Input
+// ie) Row 4 Column 4.
+
+/*
+
+ * # # #
+ * * # #
+ * * * #
+ * * * *
+
+ */
+package com.Pattern;
+
+import java.util.Scanner;
+
+public class Pattern16 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter number of Rows : ");
+        int iRow = input.nextInt();
+
+        System.out.print("Enter number of Column : ");
+        int iColumn = input.nextInt();
+
+        displayPattern(iRow,iColumn);
+
+    }
+
+    public static void displayPattern(int iRow, int iColumn) {
+        for (int i = 1; i<=iRow; i++){
+            for(int j = 1; j<=iColumn; j++){
+                if(i>=j){
+                    System.out.print("*\t");
+                }
+                else {
+                    System.out.print("#\t");
+                }
+            }
+            System.out.println();
+    }
+
+}
+
+}
+
