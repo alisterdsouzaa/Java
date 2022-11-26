3a38fa07f8fb6c2b05ab5d392efdd56de6658e62
Alister <alisterdsouzaa@outlook.com>
2022-11-26 22:04:53 +0530

    Update CommandLine.java

--- Java/CommandLine.java
+++ Java/CommandLine.java
@@ -5,7 +5,8 @@ public class CommandLine {
         System.out.println("Length of arguments in command line is :"+args.length );
         System.out.println(args[0]);
         System.out.println(args[1]);
-
+        
+        // Addition using command line arguments
         int Ans = Integer.parseInt(args[0]) + Integer.parseInt(args[1]);
         System.out.println(Ans);
 
