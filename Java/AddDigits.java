f740b8a110fc260f245d0a62b01c05c0670959e0
Alister <alisterdsouzaa@outlook.com>
2022-12-01 19:55:45 +0530

    Ceiling of a num

---
+++ Java/AddDigits.java
@@ -0,0 +1,28 @@
+/*
+    Problem Statement : Add each digit of the inputted number.
+ */
+package Java;
+
+import java.util.Scanner;
+
+public class AddDigits {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int iNo = input.nextInt();
+
+        int iResult = countDigits(iNo);
+        System.out.println(iResult);
+
+    }
+
+    private static int countDigits(int iNo){
+        int iAdd = 0;
+        while (iNo!= 0){
+            iAdd =iAdd + iNo%10;
+            iNo = iNo/10;
+        }
+        return iAdd;
+
+    }
+
+}
---
+++ Java/CountDigits.java
@@ -0,0 +1,27 @@
+/*
+  Counts the total number of digits entered by user in the number.
+ */
+package Java;
+import java.util.Scanner;
+
+public class CountDigits {
+    public static void main(String[] args) {
+            Scanner input = new Scanner(System.in);
+        System.out.println("Enter number :");
+            int iNo = input.nextInt();
+
+            int iResult = countDigits(iNo);
+            System.out.println(iResult);
+        }
+
+        private static int countDigits(int iNo){
+            int iAdd = 0;
+            while (iNo!= 0){
+                iNo = iNo/10;
+                iAdd++;
+            }
+            return iAdd;
+
+        }
+
+    }
